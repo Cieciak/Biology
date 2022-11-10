ed5c7c2970dec9f8581209e9e9bd770c27fd1526
Cieciak <nikodemrzasa@wp.pl>
2022-11-10 01:34:16 +0100

    add: better UX

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from collections import namedtuple
-import csv
+import csv, sys
+
+
+args = sys.argv
+args.extend([''] * 10)
+_, GEN_PATH, PROTEIN_PATH, *_ = args
 
 ChargaffRule = namedtuple('Chargaff', ['name', 'complementary'])
+Names = namedtuple('Name', ['en', 'pl'])
+
+
+GENETIC_SEQUENCE = 'CTTGCGACGTTG'
+
+CODONS_CSV = './csv/codons.csv'
+NAMES_CSV  = './csv/names.csv'
+AMINO_ACIDS: dict[str, Names] = {}
+
+with open(NAMES_CSV) as file:
+    names = csv.reader(file)
+    for row in names:
+        if row: 
+            amino_acid = Names(row[1].strip(), row[2].strip())
+            AMINO_ACIDS[row[0].strip()] = amino_acid
+
+if GEN_PATH:
+    with open(GEN_PATH) as file:
+        GENETIC_SEQUENCE = file.read()
+else:
+    GENETIC_SEQUENCE = input("Enter genetic sequence: ")
 
 class DNA:
 
@@ -42,7 +68,7 @@ class DNA:
         self.template_strand = DNA.create_complementary(sequence)
 
     def __repr__(self) -> str:
-        return f'{self.coding_strand}\n{self.template_strand}'
+        return f'Coding strand:   {self.coding_strand}\nTemplate strand: {self.template_strand}'
 
 class RNA:
 
@@ -67,9 +93,15 @@ class RNA:
         self.trasported_strand: str = RNA.create_complementary(sequence)
 
     def __repr__(self) -> str:
-        return self.trasported_strand
+        return f'mRNA:            {self.trasported_strand}'
 
 class Compiler:
+
+    @staticmethod
+    def to_pl(name: str) -> str: return AMINO_ACIDS[name].pl
+
+    @staticmethod
+    def to_en(name: str) -> str: return AMINO_ACIDS[name].en
 
     def __init__(self, path: str):
 
@@ -80,27 +112,39 @@ class Compiler:
             for row in genetic_code:
                 if row: self.genetic_code[row[0]] = row[1].strip()
 
-    def compile(self, rna: RNA):
+    def compile(self, rna: RNA, post = lambda x: x):
         rest = rna.trasported_strand
         aminoacid = []
         while rest:
             codon = rest[:3]
             rest = rest[3:]
 
-            aminoacid.append(self.genetic_code[codon].capitalize())
+            aminoacid.append(post(self.genetic_code[codon]))
+
 
         return '-'.join(aminoacid)
 
 
 if __name__ == '__main__':
-    dna = DNA('CTTGCGACGTTG')
+    dna = DNA(GENETIC_SEQUENCE)
 
     rna = RNA(dna)
 
-    cmp = Compiler('./codons.csv')
+    cmp = Compiler(CODONS_CSV)
 
+
+    print("DNA:")
     print(dna)
 
+    print()
+    print("RNA: ")
     print(rna)
 
-    print(cmp.compile(rna))
+    compiled = cmp.compile(rna, Compiler.to_pl).replace('_', ' ')
+
+    if PROTEIN_PATH:
+        with open(PROTEIN_PATH, 'w') as file:
+            file.write(compiled.replace('-', '\n'))
+    else:
+        print("Compiled: ")
+        print(compiled)
